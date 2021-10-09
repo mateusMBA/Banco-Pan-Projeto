09408ecfdc6c042496b2425b675a6144a8d3add8
Mateus Almeida <mateus.barros.almeida@gmail.com>
2021-10-09 15:15:15 -0300

    alterando classe conta

--- src/Conta.java
+++ src/Conta.java
@@ -2,8 +2,11 @@
 public class Conta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		double saldo;
+		int agencia;
+		int numeroConta;
+		String titular;
 	}
 
 }
